f998d01fcef0ca423d94fe5dbc1263f2bbbf2007
안훈기 <angora@anhungiui-MacBookAir.local>
2025-11-28 02:33:45 +0900

    :recycle:Refactor: exhibition 엔티티 수정& dto 수정

--- src/main/java/com/be/pbl/domain/exhibition/dto/response/ExhibitionInfoResponse.java
+++ src/main/java/com/be/pbl/domain/exhibition/dto/response/ExhibitionInfoResponse.java
@@ -2,9 +2,13 @@ package com.be.pbl.domain.exhibition.dto.response;
 
 import com.be.pbl.domain.exhibition.entity.Tag;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Getter
@@ -24,8 +28,17 @@ public class ExhibitionInfoResponse {
     @Schema(description = "작가 이름")
     private String author;
 
-    @Schema(description = "운영 시간")
-    private String operatingHours;
+    @Schema(description = "전시 시작일")
+    private LocalDate startDate;
+
+    @Schema(description = "전시 종료일")
+    private LocalDate endDate;
+
+    @Schema(description = "시작 시간")
+    private LocalTime openTime;
+
+    @Schema(description = "마감 시간")
+    private LocalTime closeTime;
 
     @Schema(description = "작품 태그")
     private List<ExhibitionTag> tags;
