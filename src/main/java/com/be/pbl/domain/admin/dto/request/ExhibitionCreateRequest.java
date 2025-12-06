a346eb26089c3045110a5241a11da78c3957ae61
imjuyongp <imjuyongp@gmail.com>
2025-12-06 19:09:10 +0900

    :recycle:Refactor: 전시회 생성 dto 요청 예시 수정

--- src/main/java/com/be/pbl/domain/admin/dto/request/ExhibitionCreateRequest.java
+++ src/main/java/com/be/pbl/domain/admin/dto/request/ExhibitionCreateRequest.java
@@ -20,46 +20,41 @@ import java.util.List;
 @Schema(description = "전시회 생성 요청 DTO")
 public class ExhibitionCreateRequest {
 
-    @NotBlank(message = "전시회 제목은 필수입니다.")
     @Schema(description = "전시회 제목", example = "모네: 빛의 순간들")
     private String title;
 
     @Schema(description = "전시회 설명", example = "인상주의의 거장 클로드 모네의 대표작들을 한자리에서 만나볼 수 있는 특별 전시")
     private String description;
 
-    @NotBlank(message = "전시회 주소는 필수입니다.")
     @Schema(description = "전시회 주소", example = "서울특별시 종로구 삼청로 30")
     private String address;
 
     @Schema(description = "작가명", example = "클로드 모네")
     private String author;
 
-    @NotNull(message = "전시 시작일은 필수입니다.")
     @Schema(description = "전시 시작일", example = "2025-01-01")
     private LocalDate startDate;
 
-    @NotNull(message = "전시 종료일은 필수입니다.")
     @Schema(description = "전시 종료일", example = "2025-03-31")
     private LocalDate endDate;
 
-    @NotNull(message = "오픈 시간은 필수입니다.")
     @Schema(description = "오픈 시간", example = "10:00:00")
     private LocalTime openTime;
 
-    @NotNull(message = "종료 시간은 필수입니다.")
     @Schema(description = "종료 시간", example = "18:00:00")
     private LocalTime closeTime;
 
-    @Schema(description = "태그 리스트", example = "[\"MODERN\", \"COLORFUL\"]")
+    @Schema(description = "태그 리스트", example = "[\"MODERN\", \"BRIGHT\"]")
     private List<Tag> tags;
 
     @Schema(description = "조회수 (기본값: 0)", example = "0")
     private String views;
 
-    @Schema(description = "이미지 URL 리스트", example = "[\"https://example.com/image1.jpg\", \"https://example.com/image2.jpg\"]")
+    @Schema(description = "이미지 URL 리스트",
+        example = "[\"https://www.insaartcenter.com/data/file/exhibition_current/f908992e10ec9c87f45e0f975130eaa9_XU48dzVM_63cc0f9944eb4da15b1a8f18eea32a1c941c3d25.jpg\", " +
+            "\"https://www.insaartcenter.com/data/file/exhibition_current/f908992e10ec9c87f45e0f975130eaa9_eJQPS3Kz_894e3d3bd08f3f7153ebc9d27d4b676538b8cb6e.jpg\"]")
     private List<String> imageUrls;
 
-    @NotBlank(message = "갤러리명은 필수입니다.")
     @Schema(description = "갤러리명", example = "국립현대미술관")
     private String galleryName;
 
