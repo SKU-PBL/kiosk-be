511b6a2d334a1474a76ce5a7b6fcfd14b2ff74bd
imjuyongp <imjuyongp@gmail.com>
2025-12-07 00:18:18 +0900

    :wrench:Setting: submodule push

---
+++ src/main/java/com/be/pbl/global/config/GptConfig.java
@@ -0,0 +1,37 @@
+package com.example.bgbg.recommend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Getter;
+
+@Configuration
+@Getter
+public class GptConfig {
+
+    @Value("${openai.api.secret-key}")
+    private String secretKey;
+
+    @Value("${openai.api.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String url;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Jackson ObjectMapper를 사용하는 메시지 컨버터 명시적 설정
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        restTemplate.getMessageConverters().add(0, converter);
+
+        return restTemplate;
+    }
+}
