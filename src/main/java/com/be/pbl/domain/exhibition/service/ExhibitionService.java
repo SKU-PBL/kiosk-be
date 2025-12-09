31d5312b4a28d99354d3efa0e928443f877ef182
안훈기 <angora@anhungiui-MacBookAir.local>
2025-12-09 18:10:09 +0900

    :sparkles:Feat: 전시회 수정 api 추가

--- src/main/java/com/be/pbl/domain/exhibition/service/ExhibitionService.java
+++ src/main/java/com/be/pbl/domain/exhibition/service/ExhibitionService.java
@@ -1,8 +1,10 @@
 package com.be.pbl.domain.exhibition.service;
 
+import com.be.pbl.domain.exhibition.dto.request.ExhibitionPatchRequest;
 import com.be.pbl.domain.exhibition.dto.response.ExhibitionInfoResponse;
 import com.be.pbl.domain.exhibition.dto.response.ExhibitionRecommendResponse;
 import com.be.pbl.domain.exhibition.dto.response.TopTagResponse;
+import com.be.pbl.domain.exhibition.entity.Genre;
 import com.be.pbl.domain.question.dto.request.QuestionAnswerListRequest;
 
 import java.util.List;
@@ -11,4 +13,7 @@ public interface ExhibitionService {
     ExhibitionInfoResponse getExhibition(Long id); // 전시회 단일 조회
     List<ExhibitionInfoResponse> getAllExhibition(); // 전시회 정보 전체 조회
     ExhibitionRecommendResponse recommendExhibitions(QuestionAnswerListRequest request); // 이상형 월드컵 응답 기반 전시회 3개 추천 메서드
+    List<ExhibitionInfoResponse> getExhibitionsByGenre(Genre genre);
+    void updateExhibition(Long id, ExhibitionPatchRequest request);
+
 }
